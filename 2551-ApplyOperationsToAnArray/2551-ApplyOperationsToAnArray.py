fdb57610aaaf5686e80cdb6342bbb021a8c5ce33
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:24:26 -0700

    Create 2551-ApplyOperationsToAnArray

---
+++ 2551-ApplyOperationsToAnArray/2551-ApplyOperationsToAnArray.py
@@ -0,0 +1,19 @@
+# Last updated: 8/1/2025, 6:24:25 PM
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        modified_nums = []
+
+        for index in range(0, n - 1):
+            if (nums[index] == nums[index + 1]) and (nums[index] != 0):
+                nums[index] *= 2
+                nums[index + 1] = 0
+
+        for num in nums:
+            if num != 0:
+                modified_nums.append(num)
+
+        while len(modified_nums) < n:
+            modified_nums.append(0)
+
+        return modified_nums
