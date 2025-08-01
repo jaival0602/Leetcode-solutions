b858fa10af47832f54b2d0c78410180d434fb338
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:24:45 -0700

    Create 2137-FinalValueOfVariableAfterPerformingOperations

---
+++ 2137-FinalValueOfVariableAfterPerformingOperations/2137-FinalValueOfVariableAfterPerformingOperations.py
@@ -0,0 +1,10 @@
+# Last updated: 8/1/2025, 6:24:45 PM
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for i in operations:
+            if i[0] == "-" or i[1] == "-":
+                x -= 1
+            else:
+                x += 1
+        return x
