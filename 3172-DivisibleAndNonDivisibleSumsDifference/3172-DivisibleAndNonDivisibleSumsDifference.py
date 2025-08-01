077dcd4ee26e9efcc083f6f253ab780154bae487
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:24:02 -0700

    Create 3172-DivisibleAndNonDivisibleSumsDifference

---
+++ 3172-DivisibleAndNonDivisibleSumsDifference/3172-DivisibleAndNonDivisibleSumsDifference.py
@@ -0,0 +1,11 @@
+# Last updated: 8/1/2025, 6:24:02 PM
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        div_by_m = []
+        n_div_by_m = []
+        for i in range(1,n+1):
+            if((i%m) == 0):
+                div_by_m.append(i)
+            else:
+                n_div_by_m.append(i)
+        return sum(n_div_by_m) - sum(div_by_m)
