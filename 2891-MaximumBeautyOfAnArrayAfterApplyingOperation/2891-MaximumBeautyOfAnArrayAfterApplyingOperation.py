089de905fa2cb3bcce18200893ec1bd103765451
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:24:07 -0700

    Create 2891-MaximumBeautyOfAnArrayAfterApplyingOperation

---
+++ 2891-MaximumBeautyOfAnArrayAfterApplyingOperation/2891-MaximumBeautyOfAnArrayAfterApplyingOperation.py
@@ -0,0 +1,13 @@
+# Last updated: 8/1/2025, 6:24:06 PM
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left = 0
+        max_beauty = 0
+
+        for right in range(len(nums)):
+            while nums[right] - nums[left] > 2 * k:
+                left += 1
+
+            max_beauty = max(max_beauty, right-left+1)
+        return max_beauty
