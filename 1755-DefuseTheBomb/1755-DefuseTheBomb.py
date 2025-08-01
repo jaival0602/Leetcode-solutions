0a70d6cc80eb7382c19101262305b80b9c4a5612
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:25:40 -0700

    Create 1755-DefuseTheBomb

---
+++ 1755-DefuseTheBomb/1755-DefuseTheBomb.py
@@ -0,0 +1,14 @@
+# Last updated: 8/1/2025, 6:25:40 PM
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        result = [0] * len(code)
+        if k == 0:
+            return result
+        for i in range(len(result)):
+            if k > 0:
+                for j in range(i + 1, i + k + 1):
+                    result[i] += code[j % len(code)]
+            else:
+                for j in range(i - abs(k), i):
+                    result[i] += code[(j + len(code)) % len(code)]
+        return result
