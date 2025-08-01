1c1fae7f0fc2d4cb8ea2870051cdcbe2c78578f4
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:26:14 -0700

    Create 1018-LargestPerimeterTriangle

---
+++ 1018-LargestPerimeterTriangle/1018-LargestPerimeterTriangle.py
@@ -0,0 +1,12 @@
+# Last updated: 8/1/2025, 6:26:14 PM
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse = True)
+        n = len(nums)
+        i = 0
+
+        while i < len(nums) - 2:
+            if nums[i] < nums[i+1] + nums[i+2]:
+                return nums[i] + nums[i+1] + nums[i+2]
+            i += 1
+        return 0
