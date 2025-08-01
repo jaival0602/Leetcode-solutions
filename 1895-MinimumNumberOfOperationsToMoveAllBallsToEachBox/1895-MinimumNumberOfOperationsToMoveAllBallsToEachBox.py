cc201127536a32345403447708b5997835215c8c
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:25:26 -0700

    Create 1895-MinimumNumberOfOperationsToMoveAllBallsToEachBox

---
+++ 1895-MinimumNumberOfOperationsToMoveAllBallsToEachBox/1895-MinimumNumberOfOperationsToMoveAllBallsToEachBox.py
@@ -0,0 +1,10 @@
+# Last updated: 8/1/2025, 6:25:26 PM
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        answer = [0] * len(boxes)
+        for current_box in range(len(boxes)):
+            # If the current box contains a ball, calculate the number of moves for every box.
+            if boxes[current_box] == "1":
+                for new_position in range(len(boxes)):
+                    answer[new_position] += abs(new_position - current_box)
+        return answer       
