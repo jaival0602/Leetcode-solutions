0306846403ede63e5d91928b7a201f7ade3f96d2
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:24:09 -0700

    Create 2800-MinimumStringLengthAfterRemovingSubstrings

---
+++ 2800-MinimumStringLengthAfterRemovingSubstrings/2800-MinimumStringLengthAfterRemovingSubstrings.py
@@ -0,0 +1,13 @@
+# Last updated: 8/1/2025, 6:24:09 PM
+class Solution:
+    def minLength(self, s: str) -> int:
+                # Continue processing while "AB" or "CD" substrings exist
+        while "AB" in s or "CD" in s:
+            if "AB" in s:
+                # Remove all occurrences of "AB"
+                s = s.replace("AB", "")
+            elif "CD" in s:
+                # Remove all occurrences of "CD"
+                s = s.replace("CD", "")
+
+        return len(s)
