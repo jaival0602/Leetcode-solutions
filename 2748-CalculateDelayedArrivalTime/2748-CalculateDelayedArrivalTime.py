4e9b4899360e7e39eb0775a5678f1c6b4dc09eb5
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:24:12 -0700

    Create 2748-CalculateDelayedArrivalTime

---
+++ 2748-CalculateDelayedArrivalTime/2748-CalculateDelayedArrivalTime.py
@@ -0,0 +1,4 @@
+# Last updated: 8/1/2025, 6:24:12 PM
+class Solution:
+    def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
+        return (arrivalTime + delayedTime) % 24
