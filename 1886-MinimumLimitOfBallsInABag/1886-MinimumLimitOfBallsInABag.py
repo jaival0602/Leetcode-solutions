8c0d71f1b30a3b8a47d17f0f2c2a1036f40a45f8
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:25:29 -0700

    Create 1886-MinimumLimitOfBallsInABag

---
+++ 1886-MinimumLimitOfBallsInABag/1886-MinimumLimitOfBallsInABag.py
@@ -0,0 +1,22 @@
+# Last updated: 8/1/2025, 6:25:29 PM
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        def can_divide(max_balls):
+            ops = 0
+            for n in nums:
+                ops += ceil(n/max_balls) -1
+                if ops > maxOperations:
+                    return False
+            return True
+
+        
+        l, r = 1, max(nums)
+        res = r
+        while l < r:
+            m = l + ((r-l)//2)
+            if can_divide(m):
+                r = m
+                res = r
+            else:
+                l = m+1
+        return res
