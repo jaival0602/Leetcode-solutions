aa731a06154df8c50533ecca3527df71ad6417a3
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:24:05 -0700

    Create 2917-CountPairsWhoseSumIsLessThanTarget

---
+++ 2917-CountPairsWhoseSumIsLessThanTarget/2917-CountPairsWhoseSumIsLessThanTarget.py
@@ -0,0 +1,15 @@
+# Last updated: 8/1/2025, 6:24:05 PM
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        left, right = 0, len(nums) - 1
+        count = 0
+
+        while left < right:
+            if nums[left] + nums[right] < target:
+                count += (right - left)
+                left += 1
+            else:
+                right -= 1
+
+        return count
