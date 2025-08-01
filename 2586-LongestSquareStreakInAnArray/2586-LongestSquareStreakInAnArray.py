69767be36bd6bea0b5db9cd44c2936ccae3c0dad
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:24:22 -0700

    Create 2586-LongestSquareStreakInAnArray

---
+++ 2586-LongestSquareStreakInAnArray/2586-LongestSquareStreakInAnArray.py
@@ -0,0 +1,17 @@
+# Last updated: 8/1/2025, 6:24:21 PM
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        streak_lengths = {}
+        nums.sort()
+        
+        for number in nums:
+            root = int(number**0.5)
+            if root * root == number and root in streak_lengths:
+                streak_lengths[number] = streak_lengths[root] + 1
+            else:
+                streak_lengths[number] = 1
+
+        longest_streak = max(streak_lengths.values(), default=0)
+
+        return longest_streak if longest_streak > 1 else -1
+
