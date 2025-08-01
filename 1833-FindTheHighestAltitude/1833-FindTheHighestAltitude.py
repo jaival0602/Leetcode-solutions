3c4efb72ba350b93187e6f78b77a40542a665c4f
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:25:32 -0700

    Create 1833-FindTheHighestAltitude

---
+++ 1833-FindTheHighestAltitude/1833-FindTheHighestAltitude.py
@@ -0,0 +1,14 @@
+# Last updated: 8/1/2025, 6:25:32 PM
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        current_altitude = 0
+        # Highest altitude currently is 0.
+        highest_point = current_altitude
+        
+        for altitude_gain in gain:
+            # Adding the gain in altitude to the current altitude.
+            current_altitude += altitude_gain
+            # Update the highest altitude.
+            highest_point = max(highest_point, current_altitude)
+        
+        return highest_point
