155077e8c9613f13421b36378bdb2d97a46d9225
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:27:46 -0700

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.py
@@ -0,0 +1,10 @@
+# Last updated: 8/1/2025, 6:27:46 PM
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        
+        return profit
