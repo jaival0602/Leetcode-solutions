1b100bc605060e668e4f477c97f41eb78c0df7aa
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:23:49 -0700

    Create 3455-MinimumLengthOfStringAfterOperations

---
+++ 3455-MinimumLengthOfStringAfterOperations/3455-MinimumLengthOfStringAfterOperations.py
@@ -0,0 +1,13 @@
+# Last updated: 8/1/2025, 6:23:49 PM
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        char_frequency_map = Counter(s)
+
+        delete_count = 0
+        for frequency in char_frequency_map.values():
+            if frequency % 2 == 1:
+                delete_count += frequency - 1
+            else:
+                delete_count += frequency - 2
+
+        return len(s) - delete_count        
