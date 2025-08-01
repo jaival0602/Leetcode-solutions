540127759b26363d5c8e35fb31dac286fd25fa5a
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2025-08-01 16:24:42 -0700

    Create 2188-MinimizedMaximumOfProductsDistributedToAnyStore

---
+++ 2188-MinimizedMaximumOfProductsDistributedToAnyStore/2188-MinimizedMaximumOfProductsDistributedToAnyStore.py
@@ -0,0 +1,19 @@
+# Last updated: 8/1/2025, 6:24:42 PM
+class Solution:
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        def can_distribute(x):
+            stores = 0
+            for q in quantities:
+                stores += math.ceil(q/x)
+            return stores <= n
+
+        l, r = 1 , max(quantities)
+        while l<=r:
+            x = (l+r) // 2
+            if can_distribute(x):
+                res = x
+                r = x-1
+            else:
+                l = x+1
+
+        return res 
