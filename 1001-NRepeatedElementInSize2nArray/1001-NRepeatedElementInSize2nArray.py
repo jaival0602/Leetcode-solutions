f396c790e081e61dab54da7e0e718c794f317aa8
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2026-01-06 00:40:28 -0800

    Create 1001-NRepeatedElementInSize2nArray

---
+++ 1001-NRepeatedElementInSize2nArray/1001-NRepeatedElementInSize2nArray.py
@@ -0,0 +1,7 @@
+# Last updated: 1/6/2026, 12:40:28 AM
+class Solution(object):
+    def repeatedNTimes(self, A):
+        count = collections.Counter(A)
+        for k in count:
+            if count[k] > 1:
+                return k
