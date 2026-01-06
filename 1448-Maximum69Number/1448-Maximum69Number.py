099857291f39609cdd5cacd0764bbc7132e760ce
jaival0602 <122582800+jaival0602@users.noreply.github.com>
2026-01-06 00:40:14 -0800

    Create 1448-Maximum69Number

---
+++ 1448-Maximum69Number/1448-Maximum69Number.py
@@ -0,0 +1,9 @@
+# Last updated: 1/6/2026, 12:40:14 AM
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        s = list(str(num))
+        for i, ch in enumerate(s):
+            if ch == '6':
+                s[i] = '9'
+                break
+        return int(''.join(s))
